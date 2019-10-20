be92c8f1b8c6eddbf8e2894ab610832ba22a27ab
Edvinas Naraveckas <e.naraveckas@gmail.com>
2019-10-20 20:27:45 +0300

    Create base with one hole mesh

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,8 @@
 #include "parameters-utils.h"
 #include "timing-utils.h"
 
+namespace model = gmsh::model;
+namespace factory = gmsh::model::occ;
 
 int main(int argc, char *argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
@@ -20,38 +22,66 @@ int main(int argc, char *argv[]) {
     std::cout << "Done reading parameters. Duration: " << getDuration(startParameters) << std::endl;
 
 
-    std::cout << "Generating mesh" << std::endl;
+    std::cout << "Initializing Gmsh" << std::endl;
     auto startGeneratingMesh = std::chrono::high_resolution_clock::now();
 
-    gmsh::initialize(argc, argv);
+    gmsh::initialize();
     gmsh::option::setNumber("General.Terminal", 1);
+    gmsh::model::add("mebrane-mesh");
 
-    gmsh::model::add("test");
 
-    // add discrete surface with tag 1
-    gmsh::model::addDiscreteEntity(2, 1);
+    std::cout << "Adding base" << std::endl;
 
-    // add 4 mesh nodes
-    gmsh::model::mesh::addNodes(2, 1,
-                                {1, 2, 3, 4}, // node tags: 1, 2, 3, and 4
-                                {0., 0., 0., // coordinates of node 1
-                                 1., 0., 0., // coordinates of node 2
-                                 1., 1., 0., // ...
-                                 0., 1., 0.});
 
-    // add 2 triangles
-    gmsh::model::mesh::addElements(2, 1,
-                                   {2}, // single type : 3-node triangle
-                                   {{1, 2}}, // triangle tags: 1 and 2
-                                   {{1, 2, 3, // triangle 1: nodes 1, 2, 3
-                                            1, 3, 4}}); // triangle 2: nodes 1, 3, 4
+    int bigBox = factory::addBox(0, 0, 0, params.l, params.l, params.h);
 
-//    gmsh::write("result.msh");
+    int hole = factory::addCylinder(params.l / 2., params.l / 2., -params.h, 0, 0, params.h*3., params.r);
 
-    gmsh::finalize();
+    std::vector<std::pair<int, int> > ov;
+    std::vector<std::vector<std::pair<int, int> > > ovv;
+    factory::cut({{3, bigBox}}, {{3, hole}}, ov, ovv, 3, true, false);
+
+
+    // synchronize the CAD model with the Gmsh model
+    factory::synchronize();
+
+
+    std::cout << "Setting meshing parameters" << std::endl;
+    double lcar1 = .1;
+    double lcar2 = .01;
+    double lcar3 = .02;
+
+    model::getEntities(ov, 0);
+    model::mesh::setSize(ov, lcar1);
+
+    double eps = 0.3;
+    model::getEntitiesInBoundingBox(0.5 - eps, 0.5 - eps, -params.h - eps,
+                                    0.5 + eps, 0.5 + eps, params.h*3 + eps, ov, 0);
+    model::mesh::setSize(ov, lcar2);
+
+    model::getBoundary({{3, hole}}, ov, false, false, true);
+    model::mesh::setSize(ov, lcar3);
+
+
+
+    // generate 3D mesh
+    model::mesh::generate(3);
+
 
     std::cout << "Done generating mesh. Duration: " << getDuration(startGeneratingMesh) << std::endl;
 
 
     std::cout << "End. Total duration: " << getDuration(start) << std::endl;
+
+    // Write mesh to file
+    // auto startWriteToFile = std::chrono::high_resolution_clock::now();
+    // gmsh::write("membrane.msh");
+    // std::cout << "Done writing to file. Duration: " << getDuration(startWriteToFile) << std::endl;
+
+    // Start Gmsh GUI
+    gmsh::fltk::run();
+    // Must be called after finishing to use GmshAPI
+    gmsh::finalize();
+
+    return 0;
 }
